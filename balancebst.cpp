43fa8eaf13868f08dd1ca0a7d5c7ab625e930c96
MehdiPC <mehdi.ghemati@gmail.com>
2021-06-16 00:30:30 +0100

    Update balancebst.cpp

--- balancebst.cpp
+++ balancebst.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 
+
 struct TreeNode {
   int val;
   TreeNode *left;
@@ -12,46 +13,43 @@ struct TreeNode {
   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+vector<TreeNode*> visited = {};
 
-void helper(TreeNode* node, vector<int> visited){
+void helper(TreeNode* node){
   //left
   if(node->left != nullptr){
-    helper(node->left, visited);
+    helper(node->left);
   }
   
   //add node to visited
-  visited.push_back(node->val);
+  visited.push_back(node);
 
   //right
   if(node->right != nullptr){
-    helper(node->right, visited);
+    helper(node->right);
   }
 }
 
 
-TreeNode* constructTree(vector<int> arr, int left, int right){
+TreeNode* constructTree(int left, int right){
   if(left > right){
     return nullptr;
   }
   int mid = (left + right) / 2;
 
-  TreeNode* root = new TreeNode(arr[mid], nullptr, nullptr);
-  root->left = constructTree(arr, left, mid-1);
-  root->right = constructTree(arr, mid+1, right);
+  TreeNode* root = visited[mid];
+  root->left = constructTree(left, mid-1);
+  root->right = constructTree(mid+1, right);
   return root;
 }
 
 
 TreeNode* balanceBST(TreeNode* root) {
-  vector<int> visited;
-  helper(root, visited);
+  helper(root);
   //In order is lnr
-  
-  TreeNode newtree = *constructTree(visited, 0, visited.size() - 1);
 
-  
-  
+  TreeNode* newtree = constructTree(0, visited.size() - 1);
 
+
+  return newtree;
 }
-
-
