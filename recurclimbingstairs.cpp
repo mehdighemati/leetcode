aeb4eed2c05cb2b7cee6c707e37a34fbb1ff377b
MehdiPC <mehdi.ghemati@gmail.com>
2021-06-26 23:56:56 +0100

    Create recurclimbingstairs.cpp

---
+++ recurclimbingstairs.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+template<typename K, typename V>
+void print_map(std::unordered_map<K, V> const &m)
+{
+    for (auto const &pair: m) {
+        std::cout << "{" << pair.first << ": " << pair.second << "}\n";
+    }
+}
+
+using namespace std;
+
+unordered_map<int,int> hm = {};
+int count = 0;
+
+int recurStairs(int n){
+  if(n<= 2){
+      return n;
+  }
+  
+  //found key
+  if(hm.find(n) != hm.end()){
+    return hm[n];
+  }
+  else{
+    int leftChain = recurStairs(n-2);
+    int rightChain = recurStairs(n-1);
+
+    hm[n] = leftChain + rightChain;
+
+    return hm[n];
+    
+  }
+}
