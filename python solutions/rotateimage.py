dfe931fe65208a50376a6ad8e881e9b2ed163a87
MehdiPC <mehdi.ghemati@gmail.com>
2021-07-22 00:12:13 +0100

    Update rotateimage.py

---
+++ python solutions/rotateimage.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reflect(self, matrix):
+        for i in range(len(matrix)):
+            for j in range(len(matrix)//2):
+                matrix[i][j], matrix[i][-j-1] = matrix[i][-j-1], matrix[i][j]
+    
+
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i,n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+    def rotate(self, matrix: List[List[int]]) -> None:
+        self.transpose(matrix)
+        self.reflect(matrix)
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
